66d6d51d639793af2aea2840dd1a2960e3060811
CHARMIN-PC\Oscar David <o.d.anota@gmail.com>
2018-07-05 12:58:40 -0500

    Adding login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+
+export interface Rango {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  selectedValue = 'Centro de negocios';
+  isLinear = true;
+  firstFormGroup: FormGroup;
+
+  rangos: Rango[] = [
+    {value: 'Centro de negocios'       , viewValue: 'Centro de negocios'       },
+    {value: 'Participante'             , viewValue: 'Participante'             },
+    {value: 'Creyente'                 , viewValue: 'Creyente'                 },
+    {value: 'Constructor'              , viewValue: 'Constructor'              },
+    {value: 'Triunfador'               , viewValue: 'Triunfador'               },
+    {value: 'Lider'                    , viewValue: 'Líder'                    },
+    {value: 'Lider Bronce'             , viewValue: 'Líder Bronce'             },
+    {value: 'Lider Plata'              , viewValue: 'Líder Plata'              },
+    {value: 'Lider Oro'                , viewValue: 'Líder Oro'                },
+    {value: 'Lider Oro Ejecutivo'      , viewValue: 'Líder Oro Ejecutivo'      },
+    {value: 'Lider Rubi'               , viewValue: 'Líder Rubí'               },
+    {value: 'Lider Rubi Ejecutivo'     , viewValue: 'Líder Rubí Ejecutivo'     },
+    {value: 'Lider Esmeralda'          , viewValue: 'Líder Esmeralda'          },
+    {value: 'Lider Esmeralda Ejecutivo', viewValue: 'Líder Esmeralda Ejecutivo'},
+    {value: 'Lider Diamante'           , viewValue: 'Líder Diamante'           },
+    {value: 'Lider Diamante Ejecutivo' , viewValue: 'Líder Diamante Ejecutivo' }
+  ];
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      idCtrl: ['', Validators.required],
+      rangoCtrl: ['', Validators.required]
+    });
+  }
+
+  loginGoogle() {
+
   }
 
 }
